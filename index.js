428c967047a73b138253a95aa332ccb084cac4c9
laramontana <larabrightside@gmail.com>
2017-11-13 01:09:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+  renderRecipeForm();
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+  init();
 })
+
+function renderRecipeForm() {
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  document.getElementById("main").innerHTML += template();
+}
+
+function createRecipe() {
+  var name = document.getElementById("name").value;
+  var description = document.getElementById("description").value;
+
+  var ingredientNodes = document.getElementsByName("ingredients");
+  var ingredients = []
+  for(let i=0;i<ingredientNodes.length;i++){
+    if(ingredientNodes[i].value !== "") {
+       ingredients.push(ingredientNodes[i].value)
+    }
+  }
+
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var recipeHTML = template({name: name, description: description, ingredients: ingredients});
+  document.getElementById("main").innerHTML += recipeHTML;
+}
