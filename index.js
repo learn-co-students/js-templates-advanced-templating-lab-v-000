65392fa4291a9be2036314c3b92b91c2807f8496
Jim Stricker <jmstricker93@gmail.com>
2017-11-22 20:36:46 -0500

    basic create-recipe form is rendering

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  loadRecipeForm()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function loadRecipeForm(){
+  var main = document.querySelector("main")
+  var recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+  var result = recipeFormTemplate()
+  // debugger
+  main.innerHTML += result
+}
+
+function addIngredientField(){
+  var ingredientsFieldsDiv = document.getElementById("ingredients-fields")
+  ingredientsFieldsDiv.innerHTML += "<input type='text' name='ingredients'><br>\n"
+}
+
+function createRecipe(){
+
+}
