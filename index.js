10e16830b3750e9a90ba210b1505950689d7ceef
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-21 10:42:48 -0800

    for debugging purposes

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 function init() {
   //put any page initialization/handlebars initialization here
-}
+  Handlebars.registerHelper('displayIngredient', function() {
+    //some function here
+  });
+  Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial"));
+  Handlebars.registerPartial("recipeFormPartial", document.getElementById("recipe-form-template"));
+
+
+} // close init()
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  let recipeName = document.getElementById("name").value;
+  // let recipeDescription = document.getElementById("description").value;
+  let recipeTemplate = document.getElementById("recipe-template").innerHTML;
+  let template = Handlebars.compile(recipeTemplate);
+  debugger;
+  let html = template({name: recipeName});
+  document.getElementById("main").innerHTML = html;
+}
+
+// function createRecipe() {
+//   var recipe = getRecipeVals()
+//   var recipeTemplate = document.getElementById("recipe-template").innerHTML
+//   var template = Handlebars.compile(recipeTemplate)
+//   document.getElementById("main").innerHTML = template(recipe)
+// }
+
+function displayEditForm() {
+
+}
+
+function updateRecipe() {
+
+}
