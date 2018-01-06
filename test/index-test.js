72cac107be7d4747f943811dfbc6adefe3c53308
baschenbrenner <baschenbrenner@hotmail.com>
2018-01-05 21:28:19 -0500

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -9,6 +9,7 @@ describe('Handlebars Templates Lab', function() {
       expect(recipeForm).toExist("Must provide a form with an id of 'recipe-form'")
       expect(recipeForm).toMatch(/onsubmit="createRecipe()"/)
       var ingredients = document.getElementsByName("ingredients")
+
       expect(ingredients.length).toBeGreaterThanOrEqualTo(5)
       var nameField = document.getElementById("name")
       expect(nameField).toExist()
