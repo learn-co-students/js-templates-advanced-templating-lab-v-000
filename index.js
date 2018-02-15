d1eefefdf9122ac8f76036b566f5d3c6c4e77f31
github username <schun08@gmail.com>
2018-02-14 20:21:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  var recipe = recipeValues();
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  document.getElementsByTagName("main")[0].innerHTML = template(recipe);
+}
+
+function recipeValues() {
+  // verified works in browser console
+  var name = document.getElementById("name").value
+  var description = document.getElementById("description").value
+
+  var ingredientList = document.getElementsByName("ingredients")
+  var ingredients = []
+  for(let i = 0; i<ingredientList.length; i++){
+    if(ingredientList[i].value !== "") {
+      ingredients.push(ingredientList[i].value);
+    }
+  }
+  return {name, description, ingredients}
+}
