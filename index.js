1def380420e65375a5ca780bcc1286765037c08c
Terry Blue <terryblue@comcast.net>
2018-02-18 01:53:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function init() {
   //put any page initialization/handlebars initialization here
+
+  Handlebars.registerHelper('displayIngredient', function() {
+
+  })
+
+  Handlebars.registerPartial('recipeDetailsPartial', function() {
+
+  })
+
+  Handlebars.registerPartial('recipeFormPartial', function() {
+
+  })
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
@@ -10,7 +23,7 @@ function createRecipe() {
 }
 
 function displayEditForm() {
-
+  var recipe = document.getElementById("recipe-form-template").innerHTML
 }
 
 function updateRecipe() {
