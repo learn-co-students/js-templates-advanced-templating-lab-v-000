199b77f2243e08b7ae8819236456a65896c4998e
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-02-24 01:30:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  document.getElementsByTagName("main")[0].innerHTML = recipeFormTemplate ;
+  
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  // var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+}
