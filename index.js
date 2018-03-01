5d38782b832e4c442fbe0cd35404897d8294d999
Hannah Carnes <hecarnes@gmail.com>
2018-03-01 16:13:11 -0500

    add recipe form and recipe template

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function init() {
-  //put any page initialization/handlebars initialization here
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({
     'submitAction': 'createRecipe()'
   })
+}
+
+function createRecipe() {
+  var recipe = getRecipeVals()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
 }
 document.addEventListener("DOMContentLoaded", function (event) {
   init()
