50e2056e683b8c3e9f1e508a51f620ff30afb8cb
meq <fewell@gmail.com>
2018-03-26 08:11:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  initializePage()
+  initializeHandlebars()
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function initializeHandlebars(){
+  Handlebars.registerHelper('displayIngredient', function() {
+  })
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+}
+
+function initializePage(){
+ let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+ document.getElementsByTagName("main")[0].innerHTML += template;
+}
+
+function createRecipe() {
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+function displayEditForm() {
+}
+
+function updateRecipe(){
+}
