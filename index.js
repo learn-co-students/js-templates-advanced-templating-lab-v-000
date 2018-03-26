7430e1a6a95c55f1b7c44efd440adc5a06fbeef2
John Fewell <fewell@gmail.com>
2018-03-26 20:33:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  initializePage()
-  initializeHandlebars()
+  let template = document.getElementById("recipe-form-template").innerHTML
+  document.getElementsByTagName("main")[0].innerHTML += template;
+  Handlebars.registerHelper('displayIngredient',  function(ingredient) {
+      return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+    })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-function initializeHandlebars(){
-  Handlebars.registerHelper('displayIngredient', function() {
-  })
-
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-
-}
-
-function initializePage(){
- let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
- document.getElementsByTagName("main")[0].innerHTML += template;
-}
-
 function createRecipe() {
-  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-  var result = template(issues);
+  const ingredientsNodes = document.getElementsByName("ingredients")
+  let ingredients = []
+  ingredientsNodes.forEach(
+    function(ingredientNode) {
+      if(ingredientNode.value !== ""){
+        ingredients.push(ingredientNode.value)
+      }
+  })
+  const name = document.getElementById("recipeName").value
+  const description = document.getElementById("recipeDescription").value
+  const recipe = {name, ingredients, description}
+  const template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  const result = template(recipe);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
 
