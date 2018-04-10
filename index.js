7538bf2c7b8a099b17dc546738647ffc427ca646
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-10 21:28:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+function displayEditForm() {
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var html = template();
+}
+
+function createRecipe() {
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var html = template();
+}
+
+function updateRecipe() {
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var html = template();
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
+
+  Handlebars.registerHelper('displayIngredient', function() {
+    return Handlebars.SafeString(this.ingredients)
+  })
+
+  let recipeDetailsPartial = Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  let recipeFormPartial = Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-template").innerHTML)
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
