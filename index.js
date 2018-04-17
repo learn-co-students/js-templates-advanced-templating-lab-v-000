b1ab0e5628e0b5a99d7a99c23d88b2eab73fc177
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-04-17 01:49:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+function initForm(){
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction':'createRecipe()'})
+}
+
+function handlebarsSetup(){
+  Handlebars.registerHelper('displayIngredient', function(ingredient){
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
+  handlebarsSetup()
+  initForm()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
