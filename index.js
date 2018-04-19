d41d7b3194a8cd13639b18a152ed48bcc0657fbc
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-19 21:09:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById('recipe-form'))
+  Handlebars.registerHelper('displayIngredient', (ingredient) => {
+    return `${ingredient.name}`;
+  });
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+function createRecipe() {
+  var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
+}
+
+function displayEditForm() {
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+}
+
+function updateRecipe() {
+  var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
+}
