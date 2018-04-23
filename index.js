59c75aabd81c6ca7742c0ef83b3fd679070f2df5
Chris Boujoukos <github email address>
2018-04-23 16:44:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+  let recipeForm = document.getElementById("recipe-form-template").innerHTML;
+  document.getElementById("main").innerHTML += recipeForm;
+  //debugger;
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+
+}
