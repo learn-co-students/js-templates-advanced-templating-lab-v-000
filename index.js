d97db9191b0b576490ca1d27bec4e13d310f2d29
jacobsilver2 <jacobsilver2@mac.com>
2018-05-11 12:13:48 -0400

    wow

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+
+  function renderRecipe() {
+    var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+    var html = template({name: 'Gordon Ramsay'});
+  }
+
 }
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
