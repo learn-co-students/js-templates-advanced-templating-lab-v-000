3680c30201b25d80dc73057c61156ea1b26dc28a
jacobsilver2 <jacobsilver2@mac.com>
2018-05-11 12:21:02 -0400

    uh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function init() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
-
+  
   function renderRecipe() {
     var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
     var html = template({name: 'Gordon Ramsay'});
