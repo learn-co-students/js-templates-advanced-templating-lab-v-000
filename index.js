6d47c6c15b24fa39bf0464047200039924368341
jacobsilver2 <jacobsilver2@mac.com>
2018-05-12 09:28:17 -0400

    making progress

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function init() {
   recipeFormInit();
+  registerPartial();
 }
 
 function recipeFormInit() {
-  var recipeForm = document.getElementById('recipe-form-template').innerHTML
-  var recipeFormTemplate = Handlebars.compile(recipeForm);
+  var recipeFormHTML = document.getElementById("recipe-form-template").innerHTML
+  var recipeFormTemplate = Handlebars.compile(recipeFormHTML)
+  document.getElementById('main').innerHTML = recipeFormTemplate(recipeFormHTML)
+}
 
+function registerPartial() {
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML)
+}
+
+function getRecipeVals() {
+  var ingredientsCol = document.getElementsByName('ingredients');
+  var ingredients = [];
+  for (var i=0; i<ingredientsCol.length;i++) {
+    if (ingredientsCol[i].value !== "") {
+      ingredients.push(ingredientsCol[i].value)
+    }
+  }
+  var name = document.getElementById('name').value;
+  var description = document.getElementById('description').value;
+  var recipe = {name, ingredients, description};
+  return recipe
 }
 
 function createRecipe() {
@@ -15,9 +34,6 @@ function createRecipe() {
   document.getElementById('main').innerHTML = template(recipe)
 }
 
-function getRecipeVals() {
-
-}
 
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
