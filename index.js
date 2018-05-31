6affbed1a48631379838cda4168c02efdc87256e
MyMac <mymac@JCB.local>
2018-05-31 11:36:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+  Handlebars.registerHelper('displayIngredient', function() {
+	})
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById('recipe-form-template').innerHTML);
+  document.getElementById('main').innerHTML += document.getElementById('recipe-form-template').innerHTML
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+function displayEditForm() {
+	document.getElementById('main').innerHTML += document.getElementById('recipe-form-template').innerHTML
+}
+
+function createRecipe() {
+
+	var name = document.getElementById('name').value;
+	var description = document.getElementById('description').value;
+	var ingredients = document.getElementsByName('ingredients').value;
+
+	var recipeTemplate = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
+	var recipeHTML = recipeTemplate({'name': name, 'description': description, 'ingredients': ingredients}); 
+
+	document.getElementById('main').innerHTML += recipeHTML;
+}
+
+function updateRecipe() {
+
+}
+
+
