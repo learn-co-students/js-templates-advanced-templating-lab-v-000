76be15a301e9b7f6ab2462bd324bded0a62b818c
Dan Suarez <suarez.daniel101@gmail.com>
2018-06-07 06:34:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function init() {
   //put any page initialization/handlebars initialization here
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe(){
+  var recipe = {
+    description: 'smoothie',
+    ingredients: [
+      {name: 'milk'},
+      {name: 'banana'},
+      {name: 'honey'},
+      {name: 'peanutbutter'},
+      {name: 'blueberries'},
+    ]
+  }
+  var template = Handlebars.compile(document.getElementById("recipe-form").innerHTML);
+  var html = template(recipe);
+}
