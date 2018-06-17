ef1f941acd400804b5e6ef441b0c61ba16542d30
github username <github email address>
2018-06-17 17:12:20 -0400

    Create recipeDetailsPartial and displayIngredient custom helper.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function init() {
     //put any page initialization/handlebars initialization here
     const template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
     document.getElementById("recipe-form").innerHTML += template();
+    Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+    Handlebars.registerHelper('displayIngredient', function() {
+        return new Handlebars.SafeString("<li>" + this.name + "</li>");
+    })
 
 }
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -9,16 +13,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 })
 
 function createRecipe() {
-    //const recipe = { name: document.getElementsByName("name")[0].value }
 
     const recipe = {
         name: document.getElementsByName("name")[0].value,
-        // description: document.getElementsByName('description')[0].value,
-        // ingredients: [
-        //     { quantity: "1 cup", name: document.getElementsByName("ingredient")[0].value },
-        //     { quantity: "3 nanoliters", name: document.getElementsByName("ingredient")[1].value },
-        //     { quantity: "12", name: document.getElementsByName("ingredient")[2].value }
-        // ]
+        description: document.getElementsByName('description')[0].value,
+        ingredients: [
+            { name: document.getElementsByName("ingredients")[0].value },
+            { name: document.getElementsByName("ingredients")[1].value },
+            { name: document.getElementsByName("ingredients")[2].value },
+            { name: document.getElementsByName("ingredients")[3].value },
+            { name: document.getElementsByName("ingredients")[4].value }
+
+        ]
     }
     console.log(recipe)
     const template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
