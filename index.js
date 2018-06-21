014fdb308ae16a692765b66ccf48eea4e77a7783
badlychadly <chadmontoya21@yahoo.com>
2018-06-21 16:49:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  createRecipe()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  var recipeFormTemplate = document.getElementById('recipe-form-template').innerHTML;
+  var template = Handlebars.compile(recipeFormTemplate);
+  document.getElementsByTagName("main")[0].innerHTML += template; 
+}
