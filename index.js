ac1ed7a5263a8720d53e0a4fb388089430e72395
Dan Suarez <suarez.daniel101@gmail.com>
2018-06-28 21:02:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function init() {
   //put any page initialization/handlebars initialization here
-
+  function createRecipe() {
   var recipe = {
     description: 'smoothie',
     ingredients: [
@@ -11,6 +11,10 @@ function init() {
       {name: 'blueberries'},
     ]
   }
+};
+
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var html = template('{{description}}', '{{ingredients}}');
 
   Handlebars.registerHelper('displayIngredient', function(items) {
     return items.name
@@ -22,10 +26,7 @@ function init() {
   });
 
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-  function createRecipe() {
-    var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-    var html = template('{{description}}', '{{ingredients}}');
-  };
+
 
 
 // EXAMPLE
