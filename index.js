6d8c6ebd6510118f51b7027acc4c70fc6d5286e3
Sean Oughton <seanoughton@gmail.com>
2018-07-03 21:57:17 +0000

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ function init() {
   //put any page initialization/handlebars initialization here
   var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
 
-  var result = template;
-
-  document.getElementsbyTagName("main")[0].innerHTML += result;
+  document.getElementsByTagName("main")[0].innerHTML += template();
+  //debugger;
 }
 
 
