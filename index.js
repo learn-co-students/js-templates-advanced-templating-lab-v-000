e9d915294b23ca713e6d7f7c4ca52cf32d984907
Sean Oughton <seanoughton@gmail.com>
2018-07-03 23:32:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
 
-  document.getElementsByTagName("main")[0].innerHTML += template();
+  //this gets the template and stores it in a variable
+  //get the form and render it to the page
+  var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  //this takes the template and adds it to a node
+  document.getElementsByTagName("main")[0].innerHTML += formTemplate();
 
-//this is where the partial is coming from
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  //partial
+  Handlebars.registerPartial('recipePartial', document.getElementById("recipe-details-partial").innerHTML);
 
-  function renderMain() {
-    var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-    var html = template({ingredient: 'test ingredient'});
-  }
+  //get the recipe-template and render it to the page
+  var recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  debugger;
+
+  //Add the html to the page
+  var html = recipeTemplate({ ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+    ]
+  });
+  document.getElementsByTagName("main")[0].innerHTML += recipeTemplate();
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+//this is telling javascript that we are turning a template into a partial
+    //declaring that you are creating a partial
+//you give the partial a name ('namePartial')which will be passed into the receiving template as a variable   {{> namePartial }}
+//then you get the innerHTML of the template you are using as a partial
+  Handlebars.registerPartial('namePartial', document.getElementById("partial-template").innerHTML)
+
+//this is getting the template where you are going to place the partial
+  //this is where the partial is going
+  var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
+
+// at this point, the partial variable is in the receiving template and is waiting for the variables(data) it needs to render the innerHTML
+  //here we are placing a value for the name: variable in the receiving template
+  // this stores the html that is the result of the receiving template using the partial with the variable
+  //you have to add this html to the page
+  var html = template({name: 'Gordon Ramsay'});
+  //document.getElementsByTagName("main")[0].innerHTML += html;
+/////////////////////////////////////////////////////////////////////////////
+
+
 
 }
 
