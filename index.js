833f03bd17e175376a45dd12117e605b32dcff99
Sean Oughton <seanoughton@gmail.com>
2018-07-04 23:33:14 +0000

    Done.

--- index.js
+++ index.js
@@ -18,6 +18,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
+function catchFormValues() {
+    var name = document.getElementById("name").value;
+    var description = document.getElementById("description").value;
+    var ingredients = document.getElementsByName("ingredients");
+    // Using forEach
+    ingredientsArray = [];
+    ingredients.forEach(function(element){
+      var newObject = {ingredient: element.value}
+      ingredientsArray.push(newObject);
+    });
+
+    //Put description and ingredients in a variable hash
+    recipeDetails = {
+      recipeName: name,
+      recipeDescription: description,
+      recipeIngredients: ingredientsArray
+    }
+}
+
+function removeForm(){
+  //var parent = document.getElementsByTagName("main");
+  var recipeForm = document.getElementById("recipe-form");
+    recipeForm.remove();
+}
+
+function getRecipeTemplate() {
+  recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+}
+
+function addRecipeTemplateToDom() {
+  document.getElementsByTagName("main")[0].innerHTML += recipeTemplate(recipeDetails);
+}
+
 
 function createRecipe() {
     catchFormValues();  //gets the form values and assigns them to a global hash variable
