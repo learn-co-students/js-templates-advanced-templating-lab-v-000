49ec992b102fcadf7cd0da3e8ac1c296a9724a22
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 04:11:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,72 @@
 function init() {
   //put any page initialization/handlebars initialization here
+   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+   Handlebars.registerPartial('recipeFormPartial',document.getElementById("recipe-form-template").innerHTML);
+
+
+  Handlebars.registerHelper("displayIngredient",function(ingredient){
+    return Handlebars.SafeString("<p>{{quantity}} {{name}}</p>");
+  });
+  
+  var template=Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  
+  var dest=document.getElementById("main");
+  var result=template();
+  dest.innerHTML=result;
+ 
 }
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
+  init();
+  
+  
+  
+  
+});
+
+function createRecipe(){
+  var name=document.getElementsByName("name")[0].value;
+  var description=document.getElementsByName('description')[0].value;
+  var ings=[];
+  var ingNodes=document.getElementsByName("ingredients");
+  for(const node in ingNodes){
+    ings.push(node.value);
+  }
+
+  
+  
+  var template=Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var result=template({"name":name, "description":description, "ingredients":ings});
+  
+  document.getElementById("main").innerHTML+=result;
+  
+}
+
+
+
+function displayEditForm(){
+  var name=document.getElementsByName("name")[0].value;
+  var description=document.getElementsByName('description')[0].value;
+  var ings=[];
+  
+  
+  var template=Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var result=template({"name":name, "description":description, "ingredients":ings});
+  document.getElementById("main").innerHTML+=result;
+  
+}
+
+function updateRecipe(){
+  var name=document.getElementsByName("name")[0].value;
+  var description=document.getElementsByName('description')[0].value;
+  var ings=[];
+  
+  
+  var template=Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var result=template({"name":name, "description":description, "ingredients":ings});
+  document.getElementById("main").innerHTML+=result;
+  
+}
+
+
+
+
