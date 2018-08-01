1831e0b0e59347cc23a866b43070617c193de6f1
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-01 20:22:08 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+Handlebars.registerPartial('namePartial', document.getElementById("partial-template").innerHTML)
+function renderMain() {
+  var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
+  var html = template({name: 'Gordon Ramsay'});
+}
