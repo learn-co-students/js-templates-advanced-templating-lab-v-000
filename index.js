17e3378363c00d0593efb6a3356d5dac9c2567b0
Eric DeSimone <eric.desimone@cortland.edu>
2018-08-22 18:22:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerHelper('displayIngredient', function() {
+    return new Handlebars.SafeString(this)
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+function createRecipe(){
+
+}
