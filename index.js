50849c5cb3a3891a59c37b9d7adb99f68e4bc7dd
Victoria Meng <nights1ststar@protonmail.com>
2018-08-28 22:26:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerHelper("displayIngredient", function() {
+    return new Handlebars.SafeString("<li>" + this.name + "</li>");
+  });
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
+
+
+function createRecipe() {
+
+  const name = document.getElementById("name").value;
+  const ingredients = [];
+  const ingredientsHTML = document.getElementsByName("ingredients");
+
+  for (i = 0; i < ingredientsHTML.length; i++) {
+    ingredients.push(ingredientsHTML[i].value);
+  };
+};
+
+// Handlebars.registerPartial('recipe-details-partial', document.getElementbyId("recipe"))
