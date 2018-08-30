fd25ff3415516e47b113cfb326d2de961356ec80
taryn yaeger <tsyaeger@gmail.com>
2018-08-30 02:45:10 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,37 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+
+
+var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+var html = template(recipe);
+
+
+function getElementsByName() {
+  
+}
+
+
+function reg(){   
+var partial = Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+}  
+
+
+      
+function displayIngredients() {
+  <ul>
+    {{ #each ingredients }};
+      <li>{{ingredient}}</li>
+    {{/each}}
+  </ul>
+}
+
+      
+    
+
+
+
+
+      
