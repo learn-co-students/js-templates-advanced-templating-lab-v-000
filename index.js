f8fa48c6a6e928b0229afa6d3a7bd8df7c9cd8b1
abourke09 <aabourke@gmail.com>
2018-08-30 17:37:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-function init() {
-  //put any page initialization/handlebars initialization here
+function displayEditForm() {
+
 }
+
+function createRecipe(){
+
+}
+
+function updateRecipe(){
+
+}
+
+function init() {
+
+// var pageDiv = document.getElementById("page");
+// var pageTemplate = document.getElementById("page-template").innerHTML;
+// var pageTemplateFn = _.template(pageTemplate);
+// var pageTemplateHTML = pageTemplateFn();
+// pageDiv.innerHTML = pageTemplateHTML;
+
+  //var recipesDiv = document.getElementById("recipes");
+  var recipesTemplate = document.getElementById("recipe-form-template").innerHTML;
+  var recipesTemplateFn = Handlebars.compile(recipesTemplate);
+  var recipesTemplateHtml = recipesTemplateFn();
+  //recipesDiv.innerHTML = recipesTemplateHtml;
+  //could replace the below line with the above line
+  document.getElementsByTagName("main")[0].innerHTML = recipesTemplateHtml;
+
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
