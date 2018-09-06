25d3fbbfff13dbfe8b5f064891f9d066b7b0be8c
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-06 14:34:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 function init() {
   //put any page initialization/handlebars initialization here
   //display ingredients helper
-  Handlebars.registerHelper('displayIngredient', function () {
-
+  Handlebars.registerHelper('displayIngredient', function (ingredient) {
+    return Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
   })
 
-  Handlebars.registerPartial('recipeDetailsPartial')
-  Handlebars.registerPartial('recipeFormPartial',
-    Ingredients: <input type="textarea" name="ingredients">
-    Name: <input type="text" id="name">
-    <input type="submit">
-  )
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial"))
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form").innerHTML)
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
