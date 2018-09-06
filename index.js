369adeb555cf00b72a4a9bceaea4390d500e9290
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-06 15:30:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+function createRecipe() {
+  var recipeTemplate = Handlebars.compile(document.getElementById("recipe-template"))
+  var recipe = {
+    name: document.getElementById("name").innerHTML
+    ingredients: document.getElementById("description").innerHTML
+    ingredients: document.getElementsByName("ingredients")[0].innerHTML
+  }
+  var result = recipeTemplate(recipe)
+  document.getElementsByTagName("main")[0].innerHTML += result
+}
+
+function displayEditForm() {
+  var form = document.getElementById("recipe-form-template").innerHTML
+  document.getElementsByTagName("main")[0].innerHTML += form
+}
+
+function updateRecipe() {
+
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
   //display ingredients helper
@@ -5,9 +25,10 @@ function init() {
     return Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
   })
 
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial"))
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form").innerHTML)
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
