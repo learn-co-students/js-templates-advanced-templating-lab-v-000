5ddfe0cef471b2b207bec1b64955068c55cec6d7
Michele Conway <micheleconway@Micheles-MacBook-Pro.local>
2018-09-06 10:27:33 -0400

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  function createRecipe() {
+    var name = document.getElementById("name").value;
+    var description = document.getElementById("description").value;
+    var ingredients = document.getElementsByName("ingredients").length;
+  }
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
