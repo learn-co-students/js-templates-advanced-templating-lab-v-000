8c52fd7721cd149e753ab15388cdfadea931ff2e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-11 01:39:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerHelper('display_ingredient', function() {
+    return new Handlebars.SafeString(this.ingredient)
+})
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipeForm() {
+  let recipe = {
+    name: "",
+    description: "",
+  }
+  let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var result = template(recipe);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+function createRecipe() {
+
+}
