7a6510e1149708fd007f909aca473d0821e50a51
cbshields <cbshields@outlook.com>
2018-09-12 17:28:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
   Handlebars.registerHelper('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML += template()
+
   Handlebars.registerHelper('displayIngredient', function(ingredient) {
     return new Handlebars.SafeString('<li> + ingredient + </li>')
   })
@@ -9,11 +16,19 @@ function init() {
 function createRecipe() {
   let name = document.getElementById('name').value
   let description = document.getElementById('description').value
-  let ingredients = document.getElementsByName('ingredients')
+  let ingredientsNodes = document.getElementsByName('ingredients')
+  var ingredients = []
+  for(var i=0;i<ingredientsNodes.length;i++) {
+    if(ingredientsNodes[i].value !== "") {
+      ingredients.push(ingredientsNodes[i].value)
+    }
+  }
+
   let recipe = {name, ingredients, description}
-  let recipeTemplate = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML)
+  let recipeTemplate = Handlebars.compile(document.getElementById('recipe-template').innerHTML)
   let result = recipeTemplate(recipe)
-  document.getElementsByTagName("main")[0].innerHTML += result
+  document.getElementsByTagName("main").innerHTML += recipeTemplate(recipe)
+
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
