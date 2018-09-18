13d475866380e80e3b893cabc646168dceabc3e7
Chris Alberti <christopherjalberti@gmail.com>
2018-09-18 11:29:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-template").innerHTML)
+
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-template").innerHTML)
+
+  Handlebars.registerHelper('displayIngredient', function() {
+
+  })
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+function createRecipe() {
+  Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+}
+
+function displayEditForm() {
+  Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+}
+
+function updateRecipe() {
+  Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+}
