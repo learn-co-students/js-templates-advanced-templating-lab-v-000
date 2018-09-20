4680b0536f64f8eb2ab7cddacd2f45855b9b98f5
Veronica Bustabad <vbustabad@gmail.com>
2018-09-20 04:54:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+//put any page initialization/handlebars initialization here
+
 function init() {
-  //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
-  Handlebars.registerHelper('displayIngredient', function() {
-    // {{each ingredients}}
-    // <p>{{name}}<p>
-    // {{/each}}
-  };
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
+
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li>' + ingredient + '</li>');
+  })
 }
 
-  function createRecipe() {
-    var recipe = {
-      name: document.getElementsById("recipeName").value;
-      description: document.getElementsById("recipeDescription").value;
-      ingredients: document.getElementsById("recipeIngredients").value;
-    }
-
-    var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
-    var result = template(recipe);
-    document.getElementsByTagName("main")[0].innerHTML += result;
+function createRecipe() {
+  var recipe = {
+    name: document.getElementById("recipeName").value;
+    description: document.getElementById("recipeDescription").value;
+    ingredients: document.getElementById("recipeIngredients").value;
   }
+
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var result = template(recipe);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+function updateRecipe() {
+  createRecipe();
+}
+
+function displayEditForm() {
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
