3089440ffa2c533ecbaff274b28422be857c4ab5
Zac Willmington <zac.willmington@gmail.com>
2018-09-21 21:49:18 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+function createRecipe() {
+    debugger;
+    let recipeName = document.getElementById('description').value;
+    const ingredients = document.getElementById('ingredients[]');
+    debugger;
+    let recipeTemplate = document.getElementById('recipe-template').innerHTML;
+
+}
+
+function displayEditForm() {
+
+}
