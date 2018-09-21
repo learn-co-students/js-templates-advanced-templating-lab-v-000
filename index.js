b6d1bc0aa96db488d72efed1030b9df8e2846c6b
Monica  Karasek <mlkarasek@gmail.com>
2018-09-21 19:48:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+  Handlebars.registerHelper('displayIngredient', function (ingredient) {
+    return ingredient.name;
+  })
+
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById('recipe-form-partial').innerHTML)
+
+
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function displayEditForm() {
+  const template = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML)
+
+}
+
+function createRecipe() {
+  const template = Handlebars.compile(document.getElementById('recipe-template').innerHTML)
+}
+
+function updateRecipe() {
+  const template = Handlebars.compile(document.getElementById('recipe-template').innerHTML)
+}
