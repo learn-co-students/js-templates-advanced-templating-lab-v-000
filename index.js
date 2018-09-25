608753be012b5814c5a3a38bf4f56088c199a2ad
Jamie Brown <jmbrowncow@gmail.com>
2018-09-25 00:59:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-function init() {
-  //put any page initialization/handlebars initialization here
+
+
+function createRecipe(){
+  let recipe = {}
 }
+
+function displayEditForm(){
+
+}
+
+function updateRecipe(){
+
+}
+
+function init() {
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+  function renderMain() {
+    let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+    let html = template({name: 'Gordon Ramsay'});
+  }
+
+  Handlebars.registerHelper('displayIngredient', function(){
+
+  })
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+// let recipe = {
+//   description: 'yummy chicken noodle soup',
+//   ingredients: [
+//     {quantity: "1 cup", name: 'chicken'},
+//     {quantity: "3 nanoliters", name: 'stock'},
+//     {quantity: "12", name: 'noodles'}
+//   ]
+// }
+//
+// let template = Handlebars.compile(document.getElementById("my-template").innerHTML);
+// let html = template(recipe);
+// }
