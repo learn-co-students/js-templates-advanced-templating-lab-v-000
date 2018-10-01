d3de7731b9ec9aa77569dbd0bc217b483870f587
Katrina Brinson <kbrinso@gmail.com>
2018-10-01 19:27:31 +0000

    work in progress

--- index.js
+++ index.js
@@ -4,3 +4,41 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+// Handlebars.registerHelper('ingredient_body', function() {
+//   if(this.state === "closed") {
+//     return new Handlebars.SafeString(this.body)
+//   } else {
+//     return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+//   }
+// })
+
+Handlebars.registerPartial(recipeDetailsPartial,document.getElementById('recipe-details-partial').innerHTML);
+// Handlebars.registerPartial(displayIngredient,document.getElementById('recipe-details-partial').innerHTML);
+// 
+//
+// function displayIngredient(){
+//
+// }
+
+function createRecipe(){
+ var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
+ let ingredients = document.getElementById('ingredients').value;
+ let description = document.getElementById('description').value;
+ var data = {description: description, ingredients: ingredients}
+ var result = template(data);
+
+}
+
+function updateRecipe(){
+ var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
+
+}
+
+// function recipeDetailsPartial(){
+//
+// }
+
+function displayEditForm(){
+
+}
