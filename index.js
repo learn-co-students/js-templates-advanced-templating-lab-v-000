1df7fe476a8f0b7c7d57fb650b0713f2deb463d8
hkleindl <hkleindl@gmail.com>
2018-11-02 17:05:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+function createRecipe() {
+  let template = document.getElementById('recipe-template').innerHTML;
+  let templateFn = Handlebars.compile(template);
+}
+
+function displayEditForm() {
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  let templateFn = Handlebars.compile(template);
+}
+
+function updateRecipe() {
+  createRecipe();
+}
+
+
+
 function init() {
   let template = document.getElementById("recipe-form-template").innerHTML;
   let templateFn = Handlebars.compile(template);
 
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
-  
+
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString("<li>" + ingredient + "</li>")
+  })
+
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form").innerHTML);
   
 }
 document.addEventListener("DOMContentLoaded", function(event) {
