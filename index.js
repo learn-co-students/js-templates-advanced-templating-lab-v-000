b3d111ed8d448b26fc20faab211ce85bb45b0d3a
hkleindl <hkleindl@gmail.com>
2018-11-02 15:11:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  let templateFn = Handlebars.compile(template);
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+  
+  
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
