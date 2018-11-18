d0cb693226708cd659ce33f2197b84441f8a85fc
ElleeB <laura.trager@gmail.com>
2018-11-18 16:00:55 -0500

    Add renderMainForm, recipe-form-template, and recipe-template

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+//
 function init() {
   //put any page initialization/handlebars initialization here
+  renderMainForm()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+//
+
+function renderMainForm() {
+  let html  = document.getElementById("recipe-form-template").innerHTML
+  document.getElementById("main").innerHTML = html
+}
+
+function handleSubmit() {
+
+}
