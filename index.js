40182d1b8089dc7d66ab6504817d2d7d9a2b761c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-21 20:09:52 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function init() {
   })
 
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML)
-
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
