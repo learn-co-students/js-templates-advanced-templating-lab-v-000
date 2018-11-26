330e9b1e315080af35c2d32f78f59ef8fdea4621
github username <github email address>
2018-11-26 09:27:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function init() {
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var result = template();
+  document.getElementById("main").innerHTML = result
   //put any page initialization/handlebars initialization here
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+// function handleSubmit() {
+//   var issue = {
+//     name: ,
+//     description: ,
+//     ingredients: ,
+//   }
+ 
+//   var template = Handlebars.compile(document.getElementById("recipe-form").innerHTML);
+//   var result = template(recipe);
+//   document.getElementsByName("name", "description", "ingredients")[0].innerHTML += result;
+// }
