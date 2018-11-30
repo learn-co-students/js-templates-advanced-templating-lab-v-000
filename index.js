b075dee35a30c563d7786cb2da5ffa5a3adf46a2
github username <github email address>
2018-11-30 18:06:34 -0500

    in progress - working on functions

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ function init() {
   //put any page initialization/handlebars initialization here
   let recipeForm = document.getElementById('recipe-form-template').innerHTML;
   let template = Handlebars.compile(recipeForm);
-
   document.getElementsByTagName('main')[0].innerHTML = template();
+
+  Handlebars.registerHelper('displayIngredient', function(ingredient){
+    return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '</li>')
+  })
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML)
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
 
 
 function handleSubmit(){
@@ -17,5 +23,4 @@ function handleSubmit(){
 
 
 function displayEditForm(){
-
 }
