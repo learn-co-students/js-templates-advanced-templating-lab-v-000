4dba354c0d6156e68281fda170214bdd873cdf1e
github username <github email address>
2018-12-01 14:41:26 -0500

    finished onSubmit function

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-function init() {
-  //put any page initialization/handlebars initialization here
-  let recipeForm = document.getElementById('recipe-form-template').innerHTML;
-  let template = Handlebars.compile(recipeForm);
-  document.getElementsByTagName('main')[0].innerHTML = template();
 
+function createForm(){
+  let recipeForm = document.getElementById('recipe-form-template').innerHTML;
+  let templateFn = Handlebars.compile(recipeForm);
+  document.getElementsByTagName('main')[0].innerHTML = templateFn();
+};
+
+function setupHandlebars(){
   Handlebars.registerHelper('displayIngredient', function(ingredient){
-    return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '</li>')
+  return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '</li>')
   })
 
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML)
+  }
+
+
+function init() {
+  //put any page initialization/handlebars initialization here
+  setupHandlebars()
+  createForm()
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
@@ -16,7 +26,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 
+
 function handleSubmit(){
+  let template = document.getElementById("recipe-template").innerHTML
+  let templateFn = Handlebars.compile(template)
+
+  let name = document.getElementById("name").value
+  let description = document.getElementById("description").value
+  let allIngredients = document.getElementsByName("ingredients")
+
+  let ingredients = [];
+  for(let i=0; i<allIngredients.length; i++){
+    ingredients.push(allIngredients[i].value)
+  }
+
+  let recipe = {name, description, ingredients}
+  
+  document.getElementById('main').innerHTML = templateFn(recipe);
+
+
 
 }
 
