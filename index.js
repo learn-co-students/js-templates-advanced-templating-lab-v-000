7e81e38c41bcdf5c273434d34d776bdd46eb3eb1
github username <github email address>
2018-12-01 14:54:28 -0500

    working on displayEdit function, trying to optimize with handleSubmit function

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ function setupHandlebars(){
   }
 
 
+  // function recipeValues() {
+  //   let template = document.getElementById("recipe-template").innerHTML
+  //   let templateFn = Handlebars.compile(template)
+  //
+  //   let name = document.getElementById("name").value
+  //   let description = document.getElementById("description").value
+  //   let allIngredients = document.getElementsByName("ingredients")
+  //
+  //   let ingredients = []
+  //   for(let i=0; i<allIngredients.length; i++){
+  //     ingredients.push(allIngredients[i].value)
+  //   }
+  // }
+
+
+
 function init() {
   //put any page initialization/handlebars initialization here
   setupHandlebars()
@@ -28,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 function handleSubmit(){
+  // recipeValues()
   let template = document.getElementById("recipe-template").innerHTML
   let templateFn = Handlebars.compile(template)
 
@@ -35,20 +52,30 @@ function handleSubmit(){
   let description = document.getElementById("description").value
   let allIngredients = document.getElementsByName("ingredients")
 
-  let ingredients = [];
+  let ingredients = []
   for(let i=0; i<allIngredients.length; i++){
     ingredients.push(allIngredients[i].value)
   }
 
   let recipe = {name, description, ingredients}
-  
   document.getElementById('main').innerHTML = templateFn(recipe);
-
-
-
 }
 
 
 
 function displayEditForm(){
+  let template = document.getElementById("recipe-form-template").innerHTML
+  let templateFn = Handlebars.compile(template)
+
+  let name = document.getElementById("name").value
+  let description = document.getElementById("description").value
+  let allIngredients = document.getElementsByName("ingredients")
+
+  let ingredients = []
+  for(let i=0; i<allIngredients.length; i++){
+    ingredients.push(allIngredients[i].value)
+  }
+
+  
+
 }
