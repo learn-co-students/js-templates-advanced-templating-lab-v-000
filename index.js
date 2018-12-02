97cb94ef7afa920ca5e0565ee0ee53cce02db04c
Anu Khambete <anukhambete@gmail.com>
2018-12-02 06:48:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  let recipe = {
-      description: 'yummy chicken noodle soup',
-      ingredients: [
-        {quantity: "1 cup", name: 'chicken'},
-        {quantity: "3 nanoliters", name: 'stock'},
-        {quantity: "12", name: 'noodles'}
-      ]
-    }
+  // let recipe = {
+  //     description: 'yummy chicken noodle soup',
+  //     ingredients: [
+  //       {quantity: "1 cup", name: 'chicken'},
+  //       {quantity: "3 nanoliters", name: 'stock'},
+  //       {quantity: "12", name: 'noodles'}
+  //     ]
+  //   }
 
-    let template = document.getElementById("my-template").innerHTML;
-    let templateFn = Handlebars.compile(template);
-    let html = templateFn(recipe);
+    let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+    let html = template({});
     document.getElementsByTagName("main")[0].innerHTML += html;
 }
 document.addEventListener("DOMContentLoaded", function(event) {
