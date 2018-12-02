5019929c30dfdb2edc166a870563247926accef1
Anu Khambete <anukhambete@gmail.com>
2018-12-02 07:51:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+Handlebars.registerHelper('displayIngredient', function(ingr) {
+  //debugger
+  let ingredient = ingr;
+
+  //debugger
+  //  if(ingredient != "") {
+  //   return Handlebars.SafeString("<li name=" + "ingredients>" + ingredient + "</li>");
+  // }
+  if(ingredient != "") {
+   return new Handlebars.SafeString(
+    "<li name=\"ingredients\"" + ">" + ingredient + "</li>"
+  );
+ }
+});
+
+
 function init() {
   //put any page initialization/handlebars initialization here
-  // let recipe = {
-  //     description: 'yummy chicken noodle soup',
-  //     ingredients: [
-  //       {quantity: "1 cup", name: 'chicken'},
-  //       {quantity: "3 nanoliters", name: 'stock'},
-  //       {quantity: "12", name: 'noodles'}
-  //     ]
-  //   }
+
 
     let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
     let html = template({});
