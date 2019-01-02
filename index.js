256c317513dddee985d6696153840d28a455d1b5
SMartinSci <sm674@cornell.edu>
2019-01-02 11:19:44 -0800

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  var template = document.getElementById("recipe-form-template").innerHTML
+  var result = Handlebars.compile(template)
+  document.getElementsByTagName("main")[0].innerHTML = result({'submitAction': 'createRecipe()'})
 }
+
+function createRecipe() {
+  var recipe = getRecipe()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
+function updateRecipe(){
+  var recipe = getRecipe()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
+function displayEditForm() {
+  var name = document.getElementById("nameHeader").innerText
+  var description = document.getElementById("recipeDescription").innerText
+  var ingredientsNodes = document.getElementsByName("ingredientsList")
+  var ingredients = []
+  for(var i=0;i<ingredientsNodes.length;i++) {
+    ingredients.push(ingredientsNodes[i].innerText)
+  }
+
+  var recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
+
+  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handelbars.compile(recipeFormTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
+function getRecipeVals() {
+  var ingredientsNodes = document.getElementsByName("ingredients")
+  var ingredients = []
+  for (var i = 0; i < ingredientsNodes.length; i++) {
+    if (ingredientsNodes[i].value !== "") {
+      ingredients.push(ingredientsNodes[i].value)
+    }
+  }
+  var name = document.getElementById("name").value
+  var description = document.getElementById("description").value
+  var recipe = { name, ingredients, description }
+  return (recipe)
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
