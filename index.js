3fdca97dddf38fed1e8507314b58406a6047b9dd
rcrdlbl <bluh@outlook.com>
2019-01-06 08:16:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var recipeFormTemplate = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML)
+  var recipeForm = recipeFormTemplate()
+  document.getElementsByTagName('main')[0].innerHTML += recipeForm
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString(`<li name="ingredients">${ingredient}</li>`)
+  })
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
