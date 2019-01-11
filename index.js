ee2fee2e4ec6e6ef9eb9b199471baf19902f7d68
kachel wittig <kjwittig@gmail.com>
2019-01-10 21:01:27 -0600

    it is working in the browser

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// $ --> variables containing dom objects
+
 function init() {
   const recipeFormHTMLString = document.getElementById('recipe-form-template').innerHTML
   const formDescriberFn = Handlebars.compile(recipeFormHTMLString)
   const finishedRecipeForm = formDescriberFn({
-    name: "name",
-    description: "description",
-    repeat_array: [1,2,3,4,5],
+    nameInput: "",
+    descriptionInput: "",
+    ingredientsInputs: ["","","","",""],
   })
+    document.getElementById('main').innerHTML = finishedRecipeForm
 
    Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
    Handlebars.registerHelper('displayIngredient', function(ingredientString) {
@@ -16,27 +19,41 @@ function init() {
       return new Handlebars.SafeString("")
     }
    })
-
-  const recipeTemplateString = document.getElementById('recipe-template').innerHTML
-  const recipeDescriberFn = Handlebars.compile(recipeTemplateString)
-  const finishedRecipeHTML = recipeDescriberFn({
-    name: "name",
-    description: "description"
-  })
-  //
-  document.getElementById('main').innerHTML = finishedRecipeForm
-
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
 function handleSubmit() {
+  const recipeName = document.getElementById('name').value
+  const recipeDescription = document.getElementById('description').value
+  const ingredientsFields = Array.from(document.getElementsByName('ingredients'))
 
+  const ingredientsStrings = ingredientsFields.map($input => $input.value)
+
+  const recipeTemplateString = document.getElementById('recipe-template').innerHTML
+  const recipeDescriberFn = Handlebars.compile(recipeTemplateString)
+  const finishedRecipeHTML = recipeDescriberFn({
+    name: recipeName,
+    description: recipeDescription,
+    ingredients: ingredientsStrings,
+  })
+  document.getElementById('main').innerHTML = finishedRecipeHTML
 }
 
 function displayEditForm() {
+  const recipeName = document.getElementById('recipeName').innerText
+  const recipeDescription = document.getElementById('recipeDescription').innerText
+  const ingredientsLIs = Array.from(document.getElementsByName('ingredients'))
+  const ingredientsStrings = ingredientsLIs.map($li => $li.innerText)
+
+  console.log(recipeName, recipeDescription, ingredientsStrings);
+
   const recipeFormHTMLString = document.getElementById('recipe-form-template').innerHTML
   const formDescriberFn = Handlebars.compile(recipeFormHTMLString)
-  document.getElementById('main').innerHTML = formDescriberFn()
+  document.getElementById('main').innerHTML = formDescriberFn({
+    nameInput: recipeName,
+    descriptionInput: recipeDescription,
+    ingredientsInputs: ingredientsStrings,
+  })
 }
