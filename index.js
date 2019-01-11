058bb684db634608b360c8b7599af4503fb53252
Lucy Wall <lhwall@gmail.com>
2019-01-11 21:08:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function init() {
   //put any page initialization/handlebars initialization here
+function handleSubmit(){
+  var template = Handlebars.compile(document.getElementById("recipe-form").innerHTML)
+  var result = template()
+document.getElementsByTagName("main")[0].innerHTML += result
+
+}
+
+Handlebars.registerPartial("RecipeDetailsPartial",document.getElementById("recipe-details-partial").innerHTML)
+
+function displayEditForm(){
+
+}
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
