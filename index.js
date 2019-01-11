9c889122f4b5538c92dab9b3023a8e58f57aaf28
kachel wittig <kjwittig@gmail.com>
2019-01-10 19:15:33 -0600

    Workin

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  const recipeFormHTMLString = document.getElementById('recipe-form-template').innerHTML
+  const formDescriberFn = Handlebars.compile(recipeFormHTMLString)
+  const finishedRecipeForm = formDescriberFn({
+    name: "name",
+    description: "description",
+    repeat_array: [1,2,3,4,5],
+  })
+
+  const recipeTemplateString = document.getElementById('recipe-template').innerHTML
+  const recipeDescriberFn = Handlebars.compile(recipeTemplateString)
+  const finishedRecipeHTML = recipeDescriberFn({
+    name: "name",
+    description: "description"
+  })
+  //
+  document.getElementById('main').innerHTML = finishedRecipeForm
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function handleSubmit() {
+
+}
