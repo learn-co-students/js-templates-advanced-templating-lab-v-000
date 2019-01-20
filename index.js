d91a4205185fcc22ede093dd1d72bf6601255a42
Mark Lawrence <lawrence.ml@gmail.com>
2019-01-20 18:04:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var formTemplate = Handlebars.compile(
+    document.getElementById("recipe-form-template").innerHTML
+  );
+  document.getElementById("main").innerHTML = formTemplate({
+    ingredients: ["", "", "", "", ""]
+  });
+
+  Handlebars.registerPartial(
+    "recipeDetailsPartial",
+    document.getElementById("recipe-details-partial").innerHTML
+  );
+
+  Handlebars.registerHelper("displayIngredient", function(ingredient) {
+    return new Handlebars.SafeString(
+      '<li name="ingredients">' + ingredient + "</li>"
+    );
+  });
 }
+
+function handleSubmit() {
+  let recipe = {};
+  let nameNode = document.getElementById("name");
+  let descriptionNode = document.getElementById("description");
+  let ingredientNodes = document.getElementsByName("ingredients");
+  recipe.name = nameNode.value;
+  recipe.description = descriptionNode.value;
+  recipe.ingredients = [];
+  for (let i = 0; i < ingredientNodes.length; i++) {
+    recipe.ingredients.push(ingredientNodes[i].value);
+  }
+  let recipeTemplate = Handlebars.compile(
+    document.getElementById("recipe-template").innerHTML
+  );
+  document.getElementById("main").innerHTML = recipeTemplate(recipe);
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
+  init();
+});
