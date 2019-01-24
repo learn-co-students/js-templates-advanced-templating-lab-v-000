a4aa75f30b378a05b2255d25d976ce00cbdc32b4
Lucy Wall <lhwall@gmail.com>
2019-01-24 15:40:07 +0000

    Done.

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ document.getElementsByTagName("main")[0].innerHTML += result
 
 Handlebars.registerPartial("recipeDetailsPartial",document.getElementById("recipe-details-partial").innerHTML)
 
-function displayEditForm(){
-let template = Handlebars.compile(document.getElementById("recipe-form-template"))
-}
-
 Handlebars.registerHelper("displayIngredient", function(ingredient){
   return new Handlebars.SafeString('<li name="ingredients">' +ingredient + "</li>")
 })
@@ -40,16 +36,31 @@ document.getElementsByTagName("main")[0].innerHTML += result
 }
 
 function displayEditForm(){
-let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
-let name = document.getElementById("name").value
-let description = document.getElementById("description").value
-let ingredients1 = document.getElementById("ingredients1").value
-let ingredients2 = document.getElementById("ingredients2").value
-let ingredients3 = document.getElementById("ingredients3").value
-let ingredients4 = document.getElementById("ingredients4").value
-let ingredients5 = document.getElementById("ingredients5").value
-
+let template = document.getElementById("recipe-form-template").innerHTML
+// let name = document.getElementById("name").value
+// let description = document.getElementById("description").value
+// let ingredients = []
+// let ingredientsArray = document.getElementsByName("ingredients")
+// for (let i = 5; i < ingredientsArray.length; i++){
+//   ingredients.push(ingredientsArray[i].value)
+// }
 document.getElementsByTagName("main")[0].innerHTML += template
 
 
+}
+
+function postAgain(){
+  let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+  let recipeObject = {}
+  recipeObject.name = document.getElementById("name").value
+  recipeObject.description = document.getElementById("description").value
+    recipeObject.ingredients = []
+
+  let ingredientsArray = document.getElementsByName("ingredients")
+  for (let i = 0; i < ingredientsArray.length; i++){
+    recipeObject.ingredients.push(ingredientsArray[i].value)
+  }
+
+  let result = template(recipeObject)
+document.getElementsByTagName("main")[0].innerHTML += result
 }
