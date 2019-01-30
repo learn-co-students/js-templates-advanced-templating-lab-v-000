3aec8c1afc94599237c27a46ce71bd0179ef5bae
Bryan Hooper <bryan@lindsayfarm.net>
2019-01-30 17:27:07 -0500

    added displayIngredients custom helper and a few more tests pass

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ function init() {
 
   let template = document.getElementById("recipe-form-template").innerHTML;
   let templateFn = Handlebars.compile(template);
-  let result = templateFn();
+  let result = templateFn({ingredients: ['', '', '', '', '']});
   
   document.getElementsByTagName("main")[0].innerHTML += result; 
 
   Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial").innerHTML); 
-    
+
+  Handlebars.registerHelper("displayIngredient", function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredients">' + ingredient + "</li>");
+  });
 
 }
 
-
+function handleSubmit(recipe) {
+  let template = document.getElementById("recipe-template").innerHTML;
+  let templateFn = Handlebars.compile(template);
+  let result = templateFn({ name: recipe.name, description: recipe.description });
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
 
 
 
