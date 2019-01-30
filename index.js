4d9f3523206b1001d7418af712dbc3fd859a2ce5
Bryan Hooper <bryan@lindsayfarm.net>
2019-01-30 11:30:40 -0500

    Added form for recipes to be created - test 1 and 2 passing

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function init() {
   //put any page initialization/handlebars initialization here
+
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  let templateFn = Handlebars.compile(template);
+  let result = templateFn();
+  
+  document.getElementsByTagName("main")[0].innerHTML += result; 
+
 }
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
