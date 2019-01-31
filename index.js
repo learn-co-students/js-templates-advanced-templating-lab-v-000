51175ab2bec46584e4d9c26ba56c7bd2dcb0a82b
Bryan Hooper <bryan@lindsayfarm.net>
2019-01-31 10:14:15 -0500

    continue to struggle with handleSubmit(); focusing on issue with reload of page

--- index.js
+++ index.js
@@ -5,21 +5,31 @@ function init() {
   let templateFn = Handlebars.compile(template);
   let result = templateFn({ingredients: ['', '', '', '', '']});
   
-  document.getElementsByTagName("main")[0].innerHTML += result; 
-
   Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial").innerHTML); 
 
   Handlebars.registerHelper("displayIngredient", function(ingredient) {
     return new Handlebars.SafeString('<li name="ingredients">' + ingredient + "</li>");
   });
 
+  document.getElementsByTagName("main")[0].innerHTML += result; 
 }
 
-function handleSubmit(recipe) {
+function handleSubmit() {
   let template = document.getElementById("recipe-template").innerHTML;
   let templateFn = Handlebars.compile(template);
-  let result = templateFn({ name: recipe.name, description: recipe.description });
+  let recipeName = document.getElementById("name").value;
+  let recipeDescription = document.getElementById("description").value;
+  let recipeIngredients = document.getElementsByName("ingredients");
+  let recIngredients = []
+  for (let i=0; i<recipeIngredients.length; i++) {
+    recIngredients.push(recipeIngredients[i].value);
+  }
+
+  let result = templateFn({ name: recipeName, description: recipeDescription, ingredients: recIngredients });
   document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+function displayEditForm() {
 }
 
 
