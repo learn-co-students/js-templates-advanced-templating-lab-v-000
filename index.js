f73bd78cb8ca18fa0b618e2014b5f022c8f81396
emanuelbierman <emanuel.bierman@gmail.com>
2019-02-07 16:38:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function init() {
   //put any page initialization/handlebars initialization here
-}
+  function renderForm() {
+    let formTemplate = document.getElementById("recipe-form-template").innerHTML;
+    let compileForm = Handlebars.compile(formTemplate);
+    document.getElementById("main").innerHTML = compileForm();
+  };
+
+  renderForm();
+};
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
-})
+});
+
+function displayEditForm() {
+
+};
+function handleSubmit() {
+  Handlebars.registerHelper('displayIngredient', function() {
+    return '<li name="ingredients">Quantity: Name: </li>';
+  });
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+};
