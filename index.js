c628ce56a05d302a02909268e5669a9d60cbf02e
emanuelbierman <emanuel.bierman@gmail.com>
2019-02-08 14:09:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerHelper('displayIngredient', function(name) {
+    return '<li name="ingredients">' + name + '</li>';
+  });
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial'));
+
   function renderForm() {
-    let formTemplate = document.getElementById("recipe-form-template").innerHTML;
-    let compileForm = Handlebars.compile(formTemplate);
+    const formTemplate = document.getElementById("recipe-form-template").innerHTML;
+    const compileForm = Handlebars.compile(formTemplate);
     document.getElementById("main").innerHTML = compileForm();
   };
 
@@ -16,8 +21,10 @@ function displayEditForm() {
 
 };
 function handleSubmit() {
-  Handlebars.registerHelper('displayIngredient', function() {
-    return '<li name="ingredients">Quantity: Name: </li>';
-  });
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+  const name = document.getElementById("name").value;
+  const context = {name: name};
+
+  const recipeTemplate = document.getElementById("recipe-template").innerHTML;
+  const compileForm = Handlebars.compile(recipeTemplate);
+  document.getElementById("main").innerHTML += compileForm(context);
 };
