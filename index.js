47f2a1abba677e74265eaaf8bb966c739a0305f5
amitnyc83 <amit_nyc@live.com>
2019-02-12 15:22:13 -0500

    able to create and submit a form template for recipes

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+
+  Handlebars.registerHelper('displayIngredient', function(ingredient){
+    return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '</li>');
+  })
+
+  let formTemplate = document.getElementById('recipe-form-template').innerHTML;
+
+  let formTemplateFn = Handlebars.compile(formTemplate);
+  let ingredientValues = formTemplateFn({ingredients: ['', '', '', '', '']});
+
+   document.getElementById("main").innerHTML += ingredientValues
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
-})
+});
+
+function handleSubmit(){
+  let recipe = {};
+
+  recipe.name = document.getElementById('name').value
+  recipe.description = document.getElementById('description').value;
+
+  let ingredientNodes = document.getElementById('ingredient');
+
+  recipe.ingredients = [];
+
+  for ( var i = 0; i < ingredientNodes.length; i++){
+    recipe.ingredients.push(ingredientNodes[i].value);
+  }
+  let recipeTemplate = document.getElementBVyId('recipe-template').innerHTML;
+
+  let recipeTemplateFn = Handlebars.compile(recipeTemplate);
+
+  document.getElementById('main').innerHTML = recipeTemplateFn(recipe);
+}
