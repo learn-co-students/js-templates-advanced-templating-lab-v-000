21787c21844e43ad3a88fecc49dd1cb02e84b902
freeWillee <will.ik.lee@gmail.com>
2019-02-15 16:02:08 -0700

    Done.

--- index.js
+++ index.js
@@ -23,5 +23,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function handleSubmit() {
   // Variable declarations
+  let recipe = {};
+  let nameNode = document.getElementById('name');
+  let descriptionNode = document.getElementById('description');
+  let ingredientNodes = document.getElementsByName('ingredients');
+
+  recipe.name = nameNode.value;
+  recipe.description = descriptionNode.value;
+  recipe.ingredients = [];
+
+  for(let i = 0; i < ingredientNodes.length; i++) {
+    recipe.ingredients.push(ingredientNodes[i].value);
+  }
+
+  let recipeTemplate = document.getElementById('recipe-template').innerHTML;
+  let recipeTemplateFcn = Handlebars.compile(recipeTemplate);
   
+  document.getElementById('main').innerHTML = recipeTemplateFcn(recipe);
+}
+
+function displayEditForm() {
+  var recipe = {}
+  var nameNode = document.getElementById('recipeName');
+  var descriptionNode = document.getElementById('recipeDescription');
+  var ingredientNodes = document.getElementsByName('ingredients');
+  recipe.name = nameNode.innerHTML;
+  recipe.description = descriptionNode.innerHTML;
+  recipe.ingredients = [];
+  for(var i = 0 ; i < ingredientNodes.length ; i++) {
+    recipe.ingredients.push(ingredientNodes[i].innerHTML);
+  }
+  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML;
+  var recipeFormTemplateFn = Handlebars.compile(recipeFormTemplate);
+  document.getElementById('main').innerHTML = recipeFormTemplateFn(recipe);
 }
