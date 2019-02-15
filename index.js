0aac1db64aa1a146f931dcff282e665dd3fb84ec
emanuelbierman <emanuel.bierman@gmail.com>
2019-02-15 13:36:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerHelper('displayIngredient', function(name) {
-    return new Handlebars.SafeString('<li name="ingredients">' + name + '</li>');
-  });
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
-
   function renderForm() {
     const formTemplate = document.getElementById("recipe-form-template").innerHTML;
     const compileForm = Handlebars.compile(formTemplate);
-    const context = {ingredients: ["","","","",""]};
-    document.getElementById("main").innerHTML = compileForm(context);
+    const ingredientsContext = {ingredients: ["","","","",""]};
+    document.getElementById("main").innerHTML = compileForm(ingredientsContext);
   };
-
   renderForm();
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '</li>');
+  });
 };
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-});
 
 function displayEditForm() {
-
-};
-function handleSubmit() {
-  const name = document.getElementById("name").value;
-  const description = document.getElementById("description").value;
+  const recipeName = document.getElementById("recipeName").innerHTML;
+  const recipeDescription = document.getElementById("recipeDescription").innerHTML;
   const ingredientsNodes = document.getElementsByName('ingredients');
-  const ingredients = [];
-  Array.prototype.forEach.call(ingredientsNodes, function(ingredient, index) {
-    ingredients[index] = ingredient.value;
-  });
-  const context = {name: name, description: description, ingredients: ingredients};
+  const recipeIngredients = [];
+
+  for (let i = 0 ; i < ingredientsNodes.length ; i++) {
+    recipeIngredients.push(ingredientsNodes[i].innerHTML);
+  }
+  const recipeContext = {name: recipeName, description: recipeDescription, ingredients: recipeIngredients};
+
+  const formTemplate = document.getElementById("recipe-form-template").innerHTML;
+  const compileForm = Handlebars.compile(formTemplate);
+  document.getElementById("main").innerHTML = compileForm(recipeContext);
+};
+
+function handleSubmit() {
+  const recipeName = document.getElementById("name").value;
+  const recipeDescription = document.getElementById("description").value;
+  const ingredientsNodes = document.getElementsByName('ingredients');
+  const recipeIngredients = [];
+  // Array.prototype.forEach.call(ingredientsNodes, function(ingredient, index) {
+  //   ingredients[index] = ingredient.value;
+  // });
+  for (let i = 0 ; i < ingredientsNodes.length ; i++) {
+    recipeIngredients.push(ingredientsNodes[i].value);
+  }
+  const recipeContext = {name: recipeName, description: recipeDescription, ingredients: recipeIngredients};
 
   const recipeTemplate = document.getElementById("recipe-template").innerHTML;
   const compileForm = Handlebars.compile(recipeTemplate);
-  document.getElementById("main").innerHTML += compileForm(context);
-
-  // better solution:
-
-  // var recipe = {}
-  // var nameNode = document.getElementById('name');
-  // var descriptionNode = document.getElementById('description');
-  // var ingredientNodes = document.getElementsByName('ingredients');
-  // recipe.name = nameNode.value;
-  // recipe.description = descriptionNode.value;
-  // recipe.ingredients = [];
-  // for(var i = 0 ; i < ingredientNodes.length ; i++) {
-  //   recipe.ingredients.push(ingredientNodes[i].value);
-  // }
-  // var recipeTemplate = document.getElementById("recipe-template").innerHTML;
-  // var recipeTemplateFn = Handlebars.compile(recipeTemplate);
-  // document.getElementById('main').innerHTML = recipeTemplateFn(recipe);
+  document.getElementById("main").innerHTML = compileForm(recipeContext);
 };
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  init()
+});
