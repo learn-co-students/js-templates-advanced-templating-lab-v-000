2a0f1a9382d5b4d2c29eb610bf1d1384c9faeacc
github username <github email address>
2019-02-18 17:00:30 -0800

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+    //put any page initialization/handlebars initialization here
+    var formTemplate = document.getElementById("recipe-form-template").innerHTML;
+    var formTemplateFn = Handlebars.compile(formTemplate);
+    document.getElementById('main').innerHTML = formTemplateFn({ingredients: ['', '', '', '', '']})
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+    init()
 })
