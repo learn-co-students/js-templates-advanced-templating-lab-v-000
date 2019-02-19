a786096fae5c89f5d2553f5d470caa91524dbca0
Christopher P. Dundon <chris.dundon@gmail.com>
2019-02-18 22:30:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const db = {
+        'name': 'Place holder',
+				'description': '',
+        'ingredients': [{ingredient: ''}, {ingredient: ''}, {ingredient: ''}, {ingredient: ''}, {ingredient: ''}]
+				};
+
 function init() {
   //put any page initialization/handlebars initialization here
+	Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+
+	Handlebars.registerHelper('displayIngredient', function() {	
+		return new Handlebars.SafeString("<li name='ingredients'>" + ingredient  + "</li>");
+	})
+
+	renderRecipeForm(db);
+}
+
+function renderRecipeForm(db) {
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  let templateFunction = Handlebars.compile(template);
+  let html = templateFunction(db);
+	document.getElementById("main").innerHTML += html;
+	console.log(html);
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
