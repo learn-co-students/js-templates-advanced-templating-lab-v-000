0b881a7be886d5c80bdddaa1e9b9398794c286bf
github username <github email address>
2019-02-21 20:12:39 -0800

    Done.

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ function init() {
 
     Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
 
-    Handlebars.registerHelper('displayIngredient', function() {
-      return "Hello"
+    Handlebars.registerHelper('displayIngredient', function(ingredient) {
+      return new Handlebars.SafeString('<li name="ingredients">'+ ingredient + '</li>');
     })
 
 }
-// function displayIngredient(ingredients) {
-//   // for (ingredient of ingredients) {
-//   //   return ingredient
-//   // }
-// }
 
 
 
@@ -28,12 +23,10 @@ function handleSubmit() {
   let recipe = {
     name: document.getElementById("name").value,
     description: document.getElementById("description").value,
-    ingredients: {}
+    ingredients: document.getElementsByName("ingredients")
   }
 
   let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-
  let result = recipeTemplate(recipe);
-
  document.getElementsByTagName("main")[0].innerHTML += result;
 }
