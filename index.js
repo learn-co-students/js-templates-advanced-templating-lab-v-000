75f67a683a5d337e954da3ebb8e534acb2fae0c9
BrentBauman86 <brentbauman86@gmail.com>
2019-02-27 05:05:10 -0800

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  document.getElementsByTagName("main")[0].innerHTML += recipeFormTemplate();
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function newRecipe() {
+  var recipeName = document.getElementById("recipeName").value;
+  var recipeDescription = document.getElementById("recipeDescription").value;
+}
+
+function displayEditForm() {
+  return "hello"
+}
