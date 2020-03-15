bbe45858b2316f153f8b4fb208a4894085b84d5d
RonSala <ron.sala@gmail.com>
2020-03-15 11:09:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function init() {
   //put any page initialization/handlebars initialization here
+
+  // i added this:
+  loadRecipeForm();
+
   function loadRecipeForm() {
-    var template = Handlebars.compile(document.getElementById("recipe-form").innerHTML);
+    var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
     var result = template();
     document.getElementsByTagName("main")[0].innerHTML += result;
   }
 
   function handleSubmit(params) {
-    
+
   }
 
   function loadRecipe() {
@@ -16,6 +20,7 @@ function init() {
     document.getElementsByTagName("main")[0].innerHTML += result;
   }
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init();
 });
