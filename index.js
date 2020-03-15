f0fca812386d4bfbf6257fae7e6b71a048a83ac2
RonSala <ron.sala@gmail.com>
2020-03-15 08:31:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  function loadRecipeForm() {
+    var template = Handlebars.compile(document.getElementById("recipe-form").innerHTML);
+    var result = template();
+    document.getElementsByTagName("main")[0].innerHTML += result;
+  }
 }
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
+  init();
+});
